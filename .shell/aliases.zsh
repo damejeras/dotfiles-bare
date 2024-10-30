d492a86c7761db1ddff889541592250efb49d55d
Darius Mejeras <darius@mejeras.lt>
2024-10-30 11:21:14 +0200

    Add grep alias

--- .shell/aliases.zsh
+++ .shell/aliases.zsh
@@ -3,6 +3,7 @@ alias la='ls -la'
 alias w2="watch -n 2 "
 alias w1="watch -n 1 "
 alias k="kubectl"
+alias grep="grep --color=always"
 
 # dotfiles alias
 alias dotfiles="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME" 
