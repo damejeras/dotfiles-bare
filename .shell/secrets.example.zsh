8e1367d2df9475d82dc6e59464c8991303a029f8
Darius Mejeras <darius@mejeras.lt>
2025-01-14 11:33:28 +0200

    Add ghostty

--- .shell/secrets.example.zsh
+++ .shell/secrets.example.zsh
@@ -1,6 +1,6 @@
-export OPENAI_API_KEY=
-export ANTHROPIC_API_KEY=
-export CODESTRAL_API_KEY=
+export OPENAI_API_KEY="op://Private/openai/credential"
+export ANTHROPIC_API_KEY="op://Private/Anthropic/credential"
+export CODESTRAL_API_KEY="op://Private/Codestral API key/api_key"
 export HISTORY_LOKI_HOST=
 export HISTORY_LOKI_USERNAME=
 export HISTORY_LOKI_PASSWORD=
