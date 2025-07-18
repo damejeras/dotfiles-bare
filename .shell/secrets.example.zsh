e2951c7d018e3983345f5252af5aeb838f4edfbe
Darius Mejeras <darius@mejeras.lt>
2025-07-18 07:31:59 +0300

    Add gemini api key

--- .shell/secrets.example.zsh
+++ .shell/secrets.example.zsh
@@ -1,6 +1,7 @@
 export OPENAI_API_KEY="op://Private/openai/credential"
 export ANTHROPIC_API_KEY="op://Private/Anthropic/credential"
 export CODESTRAL_API_KEY="op://Private/Codestral API key/api_key"
+export GEMINI_API_KEY="op://Private/Gemini API Key/api_key"
 export HISTORY_LOKI_HOST=
 export HISTORY_LOKI_USERNAME=
 export HISTORY_LOKI_PASSWORD=
